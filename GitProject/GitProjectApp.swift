9b91fead8ea515d668a58e75c1d98e3f787b1857
Marina2204 <Mariska22mak22@yandex.ru>
2024-10-10 12:42:05 +0700

    Initial Commit

---
+++ GitProject/GitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitProjectApp.swift
+//  GitProject
+//
+//  Created by user on 10.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct GitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
